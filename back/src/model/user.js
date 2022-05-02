7bfbe390ea296e372d12a0e26394a02d38c3ee13
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-05-02 14:38:08 +0200

    feat: Create user model

---
+++ back/src/model/user.js
@@ -0,0 +1,28 @@
+const db = require('./database-manager')
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const user = db.define('User', {
+    userId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'user_id'
+    },
+    name: {
+        type: DataTypes.STRING(120),
+        allowNull: false,
+        field: 'name'
+    },
+    password: {
+        type: DataTypes.STRING(120),
+        allowNull: false,
+        field: 'password',
+        set(value) {
+            const hash = bcrypt.hashSync(value, parseInt(process.env.SALT));
+            this.setDataValue('password', hash);
+        }
+    }
+});
+
+module.exports = user;
---
+++ back/src/util/update-db.js
@@ -0,0 +1,9 @@
+require('../model/database-manager')
+
+require('../model/user')
+    .sync({ force: true })
+    .then(() => {
+        console.log('\x1b[32m✔ \x1b[0mModel user synchronisé')
+    }).catch((error) => {
+    console.error('\x1b[31m❌ \x1b[0mModel user non synchronisé :', error);
+})
