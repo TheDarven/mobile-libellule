c9ec2eb166cd57741f4dee7458ae55e794fc5a68
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-05-09 18:18:43 +0200

    refactor: Improve duration of update-db

--- back/src/util/update-db.js
+++ back/src/util/update-db.js
@@ -1,11 +1,15 @@
 require('../model/database-manager')
+const userModel = require('../model/user')
 
-const isTestEnv = process.env.NODE_ENV?.trim() === 'test'
+const isTestEnv = process.env.NODE_ENV?.trim() === 'test';
 
-require('../model/user')
-    .sync({ force: isTestEnv })
-    .then(() => {
-        console.log('\x1b[32m✔ \x1b[0mModel user synchronisé')
-    }).catch((error) => {
-        console.error('\x1b[31m❌ \x1b[0mModel user non synchronisé :', error);
-    })
+(async () => {
+    await userModel.sync({ force: isTestEnv })
+        .then(() => {
+            console.log('\x1b[32m✔ \x1b[0mModel user synchronisé')
+        }).catch((error) => {
+            console.error('\x1b[31m❌ \x1b[0mModel user non synchronisé :', error)
+        })
+
+    process.exit()
+})()
