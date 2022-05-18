adc932046aa34b734d08cc4ef2ea6ca8f672f2db
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-05-18 11:17:19 +0200

    feat: Add reactnavigation dependancies

--- front/App.js
+++ front/App.js
@@ -1,9 +1,11 @@
+import 'react-native-gesture-handler'; // /!\ Should be the first import /!\
 import React from 'react';
 
 import { SafeAreaView, useColorScheme } from 'react-native';
 import { RouterProvider } from './app/router-service';
 import Router from './app/component/router/Router';
 import Colors from './app/styles/colors';
+//import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
     const isDarkMode = useColorScheme() === 'dark';
