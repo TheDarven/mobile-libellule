ccd7a8a4c46006b117edb6945c809056bcbb8bb3
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-05-18 11:18:33 +0200

    feat: Add navigators

---
+++ front/app/navigator/AppNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AppDrawer from './AppDrawer';
+import LoginScreen from '../screen/login/LoginScreen';
+import SignUpScreen from '../screen/signup/SignUpScreen';
+import { withAuth } from '../context/auth-context';
+import NavigatorStyle from './NavigatorStyle';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = ({ authContext }) => {
+    return (
+        <Stack.Navigator
+            initialRouteName="Drawer"
+            screenOptions={{
+                headerTintColor: NavigatorStyle.color,
+                headerStyle: { backgroundColor: NavigatorStyle.backgroundColor }
+            }}>
+            <Stack.Screen
+                name="Drawer"
+                component={AppDrawer}
+                options={{ headerShown: false }}
+            />
+            {authContext.isAuth() ? (
+                <>
+                    <Stack.Screen name={'Profil'} component={LoginScreen} />
+                </>
+            ) : (
+                <>
+                    <Stack.Screen
+                        name={'SignIn'}
+                        component={LoginScreen}
+                        options={{ title: 'Connexion' }}
+                    />
+                    <Stack.Screen
+                        name={'SignUp'}
+                        component={SignUpScreen}
+                        options={{ title: 'Inscription' }}
+                    />
+                </>
+            )}
+        </Stack.Navigator>
+    );
+};
+
+export default withAuth(AppNavigator);
---
+++ front/app/navigator/NavigatorStyle.js
@@ -0,0 +1,6 @@
+import Colors from '../styles/colors';
+
+export default {
+    backgroundColor: Colors.warning._50,
+    color: Colors.white._0
+};
