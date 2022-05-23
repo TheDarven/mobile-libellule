4af7af02f9c7e753954dc33881d474e5d0543680
Loïc <loic.gaillard1@grenoble-inp.fr>
2022-05-23 17:32:52 +0200

    feat(user): add a randomly generated displayName when a user registers, using an external API

--- back/src/service/user-service.js
+++ back/src/service/user-service.js
@@ -33,8 +33,9 @@ async function registerUser(name, password) {
 
     // Create user
     try {
-        const user = await userModel.create({ name, password, displayName: 'DefaultDisplay' })
-
+        // Fetch user name from external API
+        const generatedName = await getRandomUsername()
+        const user = await userModel.create({ name, password, displayName: generatedName })
         return {
             response: USER_CREATED_WITH_SUCCESS,
             data: createJwtToken(user)
@@ -74,6 +75,66 @@ function createJwtToken(user) {
     const algorithm = process.env.JWT_ALGORITHM
     const expiresIn = process.env.JWT_EXPIRES_IN
     return jwt.sign({ data: { name: user.name, userId: user.userId } }, secret, { expiresIn, algorithm });
+}
+
+function getRandomUsername() {
+    // Gathering common insect names from north america
+    const filter = {
+        "classificationOptions": {
+            "includeInfraspecies": false,
+            "includeNonstandard": false,
+            "includeProvisional": false
+        },
+        "criteriaType": "species",
+        "locationOptions": {
+            "origin": "all"
+        },
+        "pagingOptions": {
+            "page": 0,
+            "recordsPerPage": 4500
+        },
+        "recordSubtypeCriteria": null,
+        "speciesTaxonomyCriteria": [
+            {
+                "informalTaxonomy": "Insects - Bees",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Flies",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Damselflies and Dragonflies",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Caddisflies, Mayflies, and Stoneflies",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Butterflies and Moths",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Beetles",
+                "paramType": "informalTaxonomy"
+            },
+            {
+                "informalTaxonomy": "Insects - Other",
+                "paramType": "informalTaxonomy"
+            }
+        ]
+    };
+    const res = fetch('https://explorer.natureserve.org/api/data/speciesSearch', {
+        method: 'POST',
+        body: JSON.stringify(filter),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    return res.then((names) => names.json().then((json) =>{
+        const index = Math.floor(Math.random() * (json.results.length));
+        const nameList = json.results.map((data) => data.primaryCommonName);
+        return nameList[index];
+    }))
 }
 
 module.exports = { loginUser, registerUser, getUserById, getUserByName, createJwtToken }
