770dc1f1fe4048150075d60cdc85b3e0fda27113
Loic Gaillard <loic.gaillard1@grenoble-inp.org>
2022-05-25 19:11:20 +0000

    Feature/sqlite db tests

--- back/src/model/database-manager.js
+++ back/src/model/database-manager.js
@@ -1,15 +1,23 @@
 const { Sequelize } = require("sequelize");
 require('../util/env-loader')
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    dialect: 'mysql',
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    logging: false,
-    define: {
-        timestamps: false
-    },
-});
+const sequelize = process.env.NODE_ENV?.trim() === 'test' ?
+    // TEST DB
+    new Sequelize({
+        dialect: 'sqlite',
+        storage: 'src/__tests__/db/test/db.sqlite',
+        logging: false,
+    })
+    // DEV & PROD DB
+    : new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+        dialect: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        logging: false,
+        define: {
+            timestamps: false
+        },
+    });
 
 sequelize.authenticate()
     .then(() => console.log('\x1b[32m✔ \x1b[0mConnexion à la base de donnée établie.'))
