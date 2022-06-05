173ff2b6d26a342311bf424570ccd5ebd325f6fc
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-06-05 17:57:47 +0200

    feat: Create date-helper

--- front/app/component/Post/PostHeader/PostDate/PostDate.js
+++ front/app/component/Post/PostHeader/PostDate/PostDate.js
@@ -3,6 +3,7 @@ import LiText from '../../../LiText/LiText';
 import Fonts from '../../../../styles/fonts';
 import { useColorScheme, View } from 'react-native';
 import { headerDateStyle, headerDateTextStyle } from './PostDateStyle';
+import { formatDate } from '../../../../util/date-helper';
 
 const PostDate = ({ date, style }) => {
     const [formattedDate, setFormattedDate] = useState(null);
@@ -10,35 +11,8 @@ const PostDate = ({ date, style }) => {
     const isDarkMode = useColorScheme() === 'dark';
 
     useEffect(() => {
-        const newDate = new Date(date);
-
-        const months = [
-            'Janvier',
-            'Février',
-            'Mars',
-            'Avril',
-            'Mai',
-            'Juin',
-            'Juillet',
-            'Août',
-            'Septembre',
-            'Octobre',
-            'Novembre',
-            'Décembre'
-        ];
-
-        setFormattedDate(
-            `${newDate.getDate()} ${
-                months[newDate.getMonth()]
-            } ${newDate.getFullYear()} à ${prefixNumber(
-                newDate.getHours()
-            )}h${prefixNumber(newDate.getUTCMinutes())}`
-        );
+        setFormattedDate(formatDate(date));
     }, [date]);
-
-    function prefixNumber(time) {
-        return `${time < 10 ? '0' : ''}${time}`;
-    }
 
     return (
         <View style={{ ...headerDateStyle(isDarkMode), ...style }}>
